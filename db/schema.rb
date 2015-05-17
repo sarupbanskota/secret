85628f29e2419b5895ff2558bc3777ecd80ac307
Sarup Banskota <sbanskota08@gmail.com>
2015-05-18 01:14:36 +0530

    Add role and name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517191708) do
+ActiveRecord::Schema.define(version: 20150517194353) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20150517191708) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
+    t.string   "first_name"
+    t.string   "family_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
