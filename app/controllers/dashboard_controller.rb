97820a404ffba0f2c0f0449d81876be433d7a52b
Sarup Banskota <sbanskota08@gmail.com>
2015-05-18 19:33:26 +0530

    Add tshirt & meal info on speakers

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,15 @@ class DashboardController < ApplicationController
 		if user_signed_in?
 			case current_user.role
 				when "admin" 
+					@no_size_count = User.where(shirt_size: "No size provided").count
+					@s_count = User.where(shirt_size: "S").count
+					@m_count = User.where(shirt_size: "M").count
+					@l_count = User.where(shirt_size: "L").count
+
+					@no_restriction_count = User.where(meal_restriction: "No Restriction").count
+					@hindu_meal_count = User.where(meal_restriction: "Hindu Meal").count
+					@muslim_meal_count = User.where(meal_restriction: "Muslim Meal").count
+					@no_fish_count = User.where(meal_restriction: "No Fish").count
 					@seminars = Seminar.all   
 					render "_admin_dashboard"
 				when "speaker"
