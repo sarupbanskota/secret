a75ac0976c5bd95c7df3364219577eb27c8cbdff
Sarup Banskota <sbanskota08@gmail.com>
2015-05-22 22:26:05 +0530

    Validate seminar description length

--- app/models/seminar.rb
+++ app/models/seminar.rb
@@ -3,4 +3,5 @@ class Seminar < ActiveRecord::Base
 	validates :headline, presence: true
 	validates :duration, presence: true
 	validates :description, presence: true
+	validates :description, length: { in: 20..1000 }
 end
