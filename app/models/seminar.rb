2041ec848faba0d49333915a2409cdd47cfccfc5
Sarup Banskota <sbanskota08@gmail.com>
2015-06-12 21:34:23 +0530

    Duration validate presence

--- app/models/seminar.rb
+++ app/models/seminar.rb
@@ -1,6 +1,7 @@
 class Seminar < ActiveRecord::Base
   belongs_to :user
 	validates :headline, presence: true
+	validates :duration, presence: true
 	validates :description, presence: true
 	validates :description, length: { in: 20..1000 }
 	validates_numericality_of :duration, only_integer: true, allow_nil: false
